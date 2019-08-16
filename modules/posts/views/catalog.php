d016a249acfdab7915412f7c5e575c4d6c2fc064
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2019-08-16 11:55:22 +0700

    Default Catalog

--- modules/posts/views/catalog.php
+++ modules/posts/views/catalog.php
@@ -5,11 +5,8 @@
 </section>
 <div class="container">
 	<div class="row">
-		<div class="col-lg-9 col-sm-12">
-			<?php $this->load->view("plugins/posts",["data" => $data->posts, "class" => 'col-lg-4 col-sm-12']);?>
-		</div>
-		<div class="col-lg-3 col-sm-12">
-			<div class="card right-alt">
+		<div class="col-lg-4 col-sm-12 hidden-xs">
+			<div class="card">
 				<div class="card-header">
 					<h5>Category</h5>
 				</div>
@@ -17,5 +14,9 @@
 				<?php echo $data->listCatalog;?>
 			</div>
 		</div>
+		<div class="col-lg-8 col-sm-12">
+			<?php $this->load->view("plugins/posts",["data" => $data->posts, "class" => 'col-lg-4 col-sm-12']);?>
+		</div>
+		
 	</div>
 </div>
