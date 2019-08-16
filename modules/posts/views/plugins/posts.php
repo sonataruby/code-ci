8de12d765110dc77afddf31173ae7ba9da722929
macq@MacQs-MacBook.(none) <macq@MacQs-MacBook.local>
2019-08-16 11:52:37 +0700

    Remove text

--- modules/posts/views/plugins/posts.php
+++ modules/posts/views/plugins/posts.php
@@ -23,7 +23,7 @@
 			  </div>
 			  <div class="card-body">
 			    <h5 class="card-title"><a href="<?php echo post_url($value->url, $value->channel);?>"><?php echo $value->name;?></a></h5>
-			    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+			   
 			    <p>
 			    	<?php foreach ($value->catalog as $keyC => $valueC) { ?>
 			    		<a class="btn btn-sm btn-outline-info" href="<?php echo catalog_url($valueC->catalog_url, @$valueC->channel);?>"><?php echo $valueC->catalog_name;?></a>
