327bd7d6fa3d2b169bec8dd24510c66178df05d2
Roller Davic <rollerdavic@gmail.com>
2019-08-17 12:35:53 +0700

    Update posts-oncepick.php

--- modules/posts/views/plugins/posts-oncepick.php
+++ modules/posts/views/plugins/posts-oncepick.php
@@ -2,7 +2,7 @@
 	<?php
 	$first = array_pop($data);
 	?>
-	<div class="col-lg-4 col-sm-12 flex-box">
+	<div class="col-lg-4 col-sm-12 flex-box mb-3">
 		<div class="card">
 		  <div class="card-header-top">
 		  <?php echo $this->components->image($first->image,["class" => "card-img-top", "alt" => $first->name]);?>
@@ -11,7 +11,7 @@
 		    <h5 class="card-title"><a href="<?php echo post_url($first->url, $first->channel);?>"><?php echo $first->name;?></a></h5>
 		    <p><i class="fa fa-phone fa-1x"></i> <a href="tel:<?php echo config_item("hotline");?>">{hotline}</a></p>
 			<p><i class="fa fa-envelope fa-1x"></i> <a href="mailto:{site_email}?subject=Contact">{site_email}</a></p>
-			
+
 		    <p>
 		    	<?php foreach ($first->catalog as $keyC => $valueC) { ?>
 		    		<a class="btn btn-sm btn-outline-info" href="<?php echo catalog_url($valueC->catalog_url, $valueC->channel);?>"><?php echo $valueC->catalog_name;?></a>
