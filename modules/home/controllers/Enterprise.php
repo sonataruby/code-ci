a4b53ffb29577ed0ac83c676894339e60b3c3352
Roller Davic <rollerdavic@gmail.com>
2019-08-23 20:54:01 +0700

    Update Enterprise.php

--- modules/home/controllers/Enterprise.php
+++ modules/home/controllers/Enterprise.php
@@ -6,7 +6,7 @@ class Enterprise extends CPEnterprise {
 	
 	public function index()
 	{
-		$robot = $this->db->get_where("reports_robots", ["bot_name" => $this->agent->robot(),"store" => DOMAIN, "language" => config_item("language")])->result();
+		$robot = $this->db->get_where("reports_robots", ["store" => DOMAIN, "language" => config_item("language")])->result();
 		$history = $this->db->order_by("view_update","DESC")->group_by("hash_connect")->limit(20)->get_where("reports_views", ["store" => DOMAIN, "language" => config_item("language"),"is_bot" => 0])->result();
 		
 		$this->view("home",["robot" => $robot, "history" => $history]);
