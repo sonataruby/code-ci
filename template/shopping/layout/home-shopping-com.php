34c2805bc80f05fd82796de456ac331d10c02bb6
Roller Davic <rollerdavic@gmail.com>
2019-08-29 22:50:12 +0700

    Redesign

--- template/shopping/layout/home-shopping-com.php
+++ template/shopping/layout/home-shopping-com.php
@@ -20,150 +20,45 @@
   
   
 
-  <section>
+      
+  
+  
+  
+
+  
+  
+
+      
+  
+  
+  
+
+  
+  
+
+  <section class="team-2" style="padding-top: 40px; padding-bottom: 40px;">
 <div class="container">
-
-  
-  <h1 class="my-4">Page Heading
-    <small>Secondary Text</small>
-  </h1>
-
-  <div class="card-group">
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-1.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-2.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-3.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-4.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-
-  <div class="card-group">
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-5.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-6.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-7.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-
-    <div class="card">
-      <img json-image="" src="/libs/builder/image/p-8.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title" json-name=""><a json-url="" href="/{channel}/{url}.html">Card title</a></h5>
-        <p class="card-text" json-description="">This card has supporting text below as a natural lead-in to additional content.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-
+<div class="row d-flex">
+<div class="col-lg-4 col-sm-12 flex-box">
+<div class="bg-light">&nbsp;</div>
 </div>
-</section>    
-  
-  
-  
-
-  
-  
-
-  <section class="slider">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4" spellcheckker="false" contenteditable="true">Text Slider<br></h2>
-          <p class="lead">This is a description for the first slide.</p>
-        </div>
-      </div>
-      <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Second Slide</h2>
-          <p class="lead">This is a description for the second slide.</p>
-        </div>
-      </div>
-      <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
-        <div class="carousel-caption d-none d-md-block">
-          <h2 class="display-4">Third Slide</h2>
-          <p class="lead">This is a description for the third slide.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-  </div>
-    
-<style>
-.carousel-item {
-  height: 100vh;
-  min-height: 350px;
-  background: no-repeat center center scroll;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-</style>  
+<div class="col-lg-8 col-sm-12">
+<ul id="myTab" class="nav nav-tabs tab-primary" role="tablist">
+<li class="nav-item"><a id="home-tab" class="nav-link active" role="tab" href="#home" data-toggle="tab" aria-controls="home" aria-selected="true">Chú ý nhất</a></li>
+<li class="nav-item"><a id="profile-tab" class="nav-link" role="tab" href="#profile" data-toggle="tab" aria-controls="profile" aria-selected="false">Bán chạy nhất</a></li>
+<li class="nav-item"><a id="contact-tab" class="nav-link" role="tab" href="#contact" data-toggle="tab" aria-controls="contact" aria-selected="false">Đang khuyến mại</a></li><li class="nav-item"><a id="contact-tab" class="nav-link" role="tab" href="#contact" data-toggle="tab" aria-controls="contact" aria-selected="false">Hàng mới nhập</a></li>
+</ul>
+<div id="myTabContent" class="tab-content">
+<div id="home" class="tab-pane fade show active" role="tabpanel" aria-labelledby="home-tab">
+<ul class="row">
+<li class="col-lg-3 col-sm-12">Item 1</li>
+<li class="col-lg-3  col-sm-12">Item 2</li>
+<li class="col-lg-3  col-sm-12">Item 3</li>
+<li class="col-lg-3  col-sm-12">Item 4</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
 </section>
